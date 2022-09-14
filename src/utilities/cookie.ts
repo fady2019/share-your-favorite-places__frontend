61c3d6553595906e25661e62ba8bbf36c7516775
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-14 23:50:23 +0200

    some utility functions have been created for managing cookies

---
+++ src/utilities/cookie.ts
@@ -0,0 +1,35 @@
+const setItem = (key: string, value: any, expDays: number = 1) => {
+    const cookie = getItem(key);
+
+    let expires = new Date(new Date().getTime() + expDays * 24 * 60 * 60 * 1000).toUTCString();
+
+    if (cookie) {
+        expires = cookie.metadata.expires;
+    }
+
+    const cookieMetadata = {
+        expires: expires,
+    };
+
+    const cookieValue = JSON.stringify({
+        value,
+        metadata: cookieMetadata,
+    });
+
+    document.cookie = `${key}=${cookieValue}; path=/; expires=${expires}`;
+};
+
+const getItem = (key: string) => {
+    const cookies = document.cookie;
+    const cookie = cookies.split(';').find((c) => c.trim().split('=')[0] === key);
+    const cookieValue = cookie?.split('=')[1];
+    return cookieValue ? JSON.parse(cookieValue) : null;
+};
+
+const removeItem = (key: string) => {
+    document.cookie = `${key}=; max-age=0`;
+};
+
+const cookieAPI = { setItem, getItem, removeItem };
+
+export default cookieAPI;
