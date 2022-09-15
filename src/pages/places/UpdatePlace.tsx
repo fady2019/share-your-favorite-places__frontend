b02fc29b81047c4eadf602ea60dcf45337ffacc1
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-15 21:25:51 +0200

    sending the request for the backend for getting the place with the specified id

--- src/pages/places/UpdatePlace.tsx
+++ src/pages/places/UpdatePlace.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import PlaceFormContainer from '../../components/places/place-form/PlaceFormContainer';
-import { PlaceFormTypeE } from '../../interfaces/places';
+import useHttp from '../../hooks/http-hook';
 
-import { DUMMY_PLACES } from './dummy-places';
+import { PlaceFormTypeE, PLaceI } from '../../interfaces/places';
+
+import PlaceFormContainer from '../../components/places/place-form/PlaceFormContainer';
 
 const UpdatePlace: React.FC = () => {
+    const { request, response } = useHttp();
+
+    const [place, setPlace] = useState<PLaceI | null>(null);
     const { placeId } = useParams<{ placeId: string }>();
 
-    const place = DUMMY_PLACES.find((place) => place.id === placeId);
+    const backendURL = process.env.REACT_APP_BACKEND_URL;
 
-    return (
-        <PlaceFormContainer
-            formType={PlaceFormTypeE.UPDATE_PLACE_FORM}
-            title={place?.title}
-            address={place?.address}
-            description={place?.description}
-            imgURL={place?.imgURL}
-        />
-    );
+    useEffect(() => {
+        if (response) {
+            setPlace(response.place);
+        }
+    }, [response]);
+
+    useEffect(() => {
+        const reqURL = `${backendURL}/places/${placeId}`;
+        request(reqURL);
+    }, [request, backendURL, placeId]);
+
+    return <>{place && <PlaceFormContainer formType={PlaceFormTypeE.UPDATE_PLACE_FORM} {...place} />}</>;
 };
 
 export default UpdatePlace;
