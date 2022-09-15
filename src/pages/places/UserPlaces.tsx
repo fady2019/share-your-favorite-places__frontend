0821fd2d3b1a4638f153669bd9948fdbd3827a76
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-15 21:27:29 +0200

    sending the request for the backend for getting the places of the specified user

--- src/pages/places/UserPlaces.tsx
+++ src/pages/places/UserPlaces.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 
-import PlacesContainer from '../../components/places/PlacesContainer';
+import useHttp from '../../hooks/http-hook';
 
-import { DUMMY_PLACES } from './dummy-places';
+import { AppStoreI } from '../../interfaces/store';
+import { PLaceI } from '../../interfaces/places';
+
+import PlacesContainer from '../../components/places/PlacesContainer';
+import { useSelector } from 'react-redux';
 
 const UserPlaces: React.FC<any> = () => {
-    const { userId } = useParams<{userId: string}>();
+    const { request, response } = useHttp();
 
-    const places = DUMMY_PLACES.filter((place) => place.creator === userId);
+    const isLoading = useSelector((state: AppStoreI) => state.ui.appLoading.isOpen);
 
-    return <PlacesContainer places={places} />;
+    const [places, setPlaces] = useState<PLaceI[]>([]);
+    const { userId } = useParams<{ userId: string }>();
+
+    const backendURL = process.env.REACT_APP_BACKEND_URL;
+
+    useEffect(() => {
+        if (response) {
+            setPlaces(response.places);
+        }
+    }, [response]);
+
+    useEffect(() => {
+        const reqURL = `${backendURL}/places/user/${userId}`;
+        request(reqURL);
+    }, [request, backendURL, userId]);
+
+    return <>{!isLoading && <PlacesContainer places={places} />}</>;
 };
 
 export default UserPlaces;
