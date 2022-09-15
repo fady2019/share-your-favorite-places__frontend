64f99ce92a31436189d43549cca7f5ecbdba933c
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-15 21:28:02 +0200

    PlaceFormContainerI interface has been adjusted

--- src/interfaces/places.ts
+++ src/interfaces/places.ts
@@ -32,6 +32,7 @@ export enum PlaceFormTypeE {
 
 export interface PlaceFormContainerI {
     formType: PlaceFormTypeE;
+    id?: string;
     title?: string;
     address?: string;
     description?: string;
