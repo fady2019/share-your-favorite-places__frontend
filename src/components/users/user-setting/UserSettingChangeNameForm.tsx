ebe11ea81b1d7963acca4752571d5ccb66a51183
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-15 21:29:45 +0200

    sending the request for the backend for updating user name

--- src/components/users/user-setting/UserSettingChangeNameForm.tsx
+++ src/components/users/user-setting/UserSettingChangeNameForm.tsx
@@ -1,7 +1,10 @@
-import React, { FormEvent } from 'react';
-import { useSelector } from 'react-redux';
+import React, { FormEvent, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { useForm } from '../../../hooks/form-hook';
+import useHttp from '../../../hooks/http-hook';
+import { uiActions } from '../../../store/slices/ui/ui-slice';
+import { userActions } from '../../../store/slices/user/user-slice';
 import { ChangeNameFormI } from '../../../interfaces/user-setting';
 import { AppStoreI } from '../../../interfaces/store';
 import { VALIDATOR_REQUIRE } from '../../../utilities/validators';
@@ -22,16 +25,41 @@ const formInitState = (initName: string = '') => {
 
 const UserSettingChangeNameForm: React.FC<any> = () => {
     const initName = useSelector((state: AppStoreI) => state.user.userInfo?.name) || 'user name';
+    const token = useSelector((state: AppStoreI) => state.auth.token);
+    const dispatch = useDispatch();
 
+    const { request, response } = useHttp();
     const { formState, getInputHandler } = useForm<ChangeNameFormI>(formInitState(initName));
 
     const isFormValid = formState.valid;
     const isChanged = formState.inputs.name.value.trim() !== initName;
 
+    const backendURL = process.env.REACT_APP_BACKEND_URL;
+
+    useEffect(() => {
+        if (response) {
+            dispatch(userActions.setUserInfo(response.user));
+            dispatch(uiActions.closeAppModal());
+        }
+    }, [response, dispatch]);
+
     const changeNameHandler = (e: FormEvent) => {
         e.preventDefault();
 
-        console.log(formState);
+        if (isFormValid) {
+            const reqURL = `${backendURL}/users/change/name`;
+
+            request(reqURL, {
+                method: 'PATCH',
+                headers: {
+                    Authorization: `Bearer ${token?.id}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: formState.inputs.name.value,
+                }),
+            });
+        }
     };
 
     return (
