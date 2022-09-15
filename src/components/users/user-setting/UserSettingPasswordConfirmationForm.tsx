af6504dacf1e75dfac0560eede298156e6adcbb6
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-15 21:30:25 +0200

    sending the request for the backend for deleting user account

--- src/components/users/user-setting/UserSettingPasswordConfirmationForm.tsx
+++ src/components/users/user-setting/UserSettingPasswordConfirmationForm.tsx
@@ -1,12 +1,16 @@
-import React, { FormEvent } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { FormEvent, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { useForm } from '../../../hooks/form-hook';
+import useHttp from '../../../hooks/http-hook';
+import { logout } from '../../../store/slices/auth/auth-slice';
+import { uiActions } from '../../../store/slices/ui/ui-slice';
+import FormInput from '../../ui/input/Input';
+import { AppStoreI } from '../../../interfaces/store';
 import { PasswordConfirmationFormI } from '../../../interfaces/user-setting';
 import { VALIDATOR_REQUIRE } from '../../../utilities/validators';
 import UserSettingForm from './UserSettingForm';
-import FormInput from '../../ui/input/Input';
-import { uiActions } from '../../../store/slices/ui/ui-slice';
 
 const formInitState = {
     inputs: {
@@ -19,14 +23,38 @@ const formInitState = {
 };
 
 const UserSettingPasswordConfirmationForm: React.FC<any> = () => {
+    const token = useSelector((state: AppStoreI) => state.auth.token);
     const dispatch = useDispatch();
-
+    const history = useHistory();
+    const { request, response } = useHttp();
     const { formState, getInputHandler } = useForm<PasswordConfirmationFormI>(formInitState);
 
     const isFormValid = formState.valid;
 
+    const backendURL = process.env.REACT_APP_BACKEND_URL;
+
+    useEffect(() => {
+        if (response) {
+            history.replace('/');
+            dispatch(logout());
+            dispatch(uiActions.closeAppModal());
+        }
+    }, [response, dispatch, history]);
+
     const closeAccountDeletionAlertHandler = (confirmation: boolean) => {
         if (confirmation && isFormValid) {
+            const reqURL = `${backendURL}/users/delete`;
+
+            request(reqURL, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${token?.id}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    password: formState.inputs.password.value,
+                }),
+            });
         }
     };
 
