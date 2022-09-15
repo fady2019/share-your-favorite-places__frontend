8aa7d4ca3e5070e1bf2d36e497ea68afef441991
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-15 21:29:27 +0200

    sending the request for the backend for updating user email

--- src/components/users/user-setting/UserSettingChangeEmailForm.tsx
+++ src/components/users/user-setting/UserSettingChangeEmailForm.tsx
@@ -1,8 +1,14 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { useForm } from '../../../hooks/form-hook';
+import useHttp from '../../../hooks/http-hook';
+import { login } from '../../../store/slices/auth/auth-slice';
+import { uiActions } from '../../../store/slices/ui/ui-slice';
+import { AppStoreI } from '../../../interfaces/store';
 import { ChangeEmailFormI } from '../../../interfaces/user-setting';
 import { VALIDATOR_EMAIL, VALIDATOR_REQUIRE } from '../../../utilities/validators';
+
 import UserSettingForm from './UserSettingForm';
 import FormInput from '../../ui/input/Input';
 
@@ -23,17 +29,43 @@ const formInitState = (initEmail: string = '') => {
 };
 
 const UserSettingChangeEmailForm: React.FC<any> = () => {
-    const initEmail = 'fady@test.com';
+    const initEmail = useSelector((state: AppStoreI) => state.user.userInfo?.email) || 'user email';
+    const token = useSelector((state: AppStoreI) => state.auth.token);
+    const dispatch = useDispatch();
 
+    const { request, response } = useHttp();
     const { formState, getInputHandler } = useForm<ChangeEmailFormI>(formInitState(initEmail));
 
     const isFormValid = formState.valid;
     const isChanged = formState.inputs.email.value.trim() !== initEmail;
 
+    const backendURL = process.env.REACT_APP_BACKEND_URL;
+
+    useEffect(() => {
+        if (response) {
+            dispatch(login(response));
+            dispatch(uiActions.closeAppModal());
+        }
+    }, [response, dispatch]);
+
     const changeNameHandler = (e: FormEvent) => {
         e.preventDefault();
 
-        console.log(formState);
+        if (isFormValid) {
+            const reqURL = `${backendURL}/users/change/email`;
+
+            request(reqURL, {
+                method: 'PATCH',
+                headers: {
+                    Authorization: `Bearer ${token?.id}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    newEmail: formState.inputs.email.value,
+                    password: formState.inputs.password.value,
+                }),
+            });
+        }
     };
 
     return (
@@ -57,9 +89,7 @@ const UserSettingChangeEmailForm: React.FC<any> = () => {
                 name="password"
                 type="password"
                 label="password"
-                validators={[
-                    VALIDATOR_REQUIRE('Password is required, please enter it!'),
-                ]}
+                validators={[VALIDATOR_REQUIRE('Password is required, please enter it!')]}
                 onGetInput={getInputHandler}
             />
         </UserSettingForm>
