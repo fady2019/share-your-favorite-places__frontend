e380d195900aa43c7605750bbbc5cb3886d6663c
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-15 16:17:09 +0200

    fetching users from backend when Users page renders

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import UsersContainer from '../../components/users/UsersContainer';
-
-import { DUMMY_USERS } from './dummy-users';
+import useHttp from '../../hooks/http-hook';
+import { AppStoreI } from '../../interfaces/store';
+import { UserI } from '../../interfaces/users';
 
 const Users: React.FC = () => {
-    return <UsersContainer users={DUMMY_USERS} />;
+    const { request, response } = useHttp();
+    const isLoading = useSelector((state: AppStoreI) => state.ui.appLoading.isOpen);
+    const [users, setUsers] = useState<UserI[]>([]);
+
+    const backendURL = process.env.REACT_APP_BACKEND_URL;
+
+    useEffect(() => {
+        if (response) {
+            setUsers(response.users);
+        }
+    }, [response]);
+
+    useEffect(() => {
+        const reqURL = `${backendURL}/users`;
+        request(reqURL);
+    }, [request]);
+
+    return <>{!isLoading && <UsersContainer users={users} />}</>;
 };
 
 export default Users;
