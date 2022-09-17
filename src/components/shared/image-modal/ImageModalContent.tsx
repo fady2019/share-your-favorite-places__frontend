a9b72a8e9fb0a5325b75dc826c3e9a23d545c47e
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-17 13:42:58 +0200

    alt attribute has been added to img tag

--- src/components/shared/image-modal/ImageModalContent.tsx
+++ src/components/shared/image-modal/ImageModalContent.tsx
@@ -5,7 +5,7 @@ import classes from './ImageModalContent.module.css';
 const ImageModalContent: React.FC<any> = (props) => {
     return (
         <div className={classes['dra-image-modal-content']}>
-            <img className={classes['img']} src={props.image} />
+            <img className={classes['img']} src={props.image} alt={props.alt} />
         </div>
     );
 };
